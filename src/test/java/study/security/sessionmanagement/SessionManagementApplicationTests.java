4b90319f361be52a75120a3978dfe27eb108efcf
hclee <heechul4296@gmail.com>
2022-10-15 21:24:25 +0900

    spring security session management source

---
+++ src/test/java/study/security/sessionmanagement/SessionManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package study.security.sessionmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SessionManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
